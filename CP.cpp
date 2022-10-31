6271b8c6b1ef85b45abe7f218ec91912473636e0
Z3RO-O <vaibhavsingh_21523@aitpune.edu.in>
2022-10-31 11:52:10 +0530

    solution

--- CP.cpp
+++ CP.cpp
@@ -8,7 +8,7 @@ using namespace std;
 // sync disables synchronization between c & c++
 // as a side effect it makes execution of code faster
 #define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
-#define testcase while (tc--)
+#define testcase while (t--)
 #define for0 for (i = 0; i < n; i++)
 #define for1 for (i = 1; i <= n; i++)
 #define vi vector<int>
@@ -16,6 +16,7 @@ using namespace std;
 #define vs vector<string>
 #define pii pair<int, int>
 #define mii map<int, int>
+#define ms map<string, int>
 #define pb push_back
 #define eb emplace_back
 #define ull unsigned long long
