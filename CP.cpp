4e73fb13f0961fd0a283d522cc7c1dcbbb8b0137
Z3RO-O <vaibhavsingh_21523@aitpune.edu.in>
2022-11-25 18:22:11 +0530

    fast added and unordered set

--- CP.cpp
+++ CP.cpp
@@ -4,11 +4,16 @@ using namespace std;
 #define main int main()
 #define sf scanf
 #define pf printf
-#define testcase while (t--)
+#define T     \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define for0 for (i = 0; i < n; i++)
 #define for1 for (j = 1; j <= n; j++)
 #define vi vector<int>
 #define si set<int>
+#define usi unordered_set<int>
 #define vs vector<string>
 #define pii pair<int, int>
 #define mii map<int, int>
@@ -23,7 +28,7 @@ const int I9 = 1e9;
 const int I7 = 1e7;
 const int I5 = 1e5;
 /* direction array */
-int dx4[] = {1, -1, 0, 0};
+/*int dx4[] = {1, -1, 0, 0};
 int dy4[] = {0, 0, 1, -1};
 int dx6[] = {0, 0, 1, -1, 0, 0};
 int dy6[] = {1, -1, 0, 0, 0, 0};
@@ -31,7 +36,7 @@ int dz6[] = {0, 0, 0, 0, 1, -1};
 int dx8[] = {1, -1, 0, 0, -1, 1, -1, 1};
 int dy8[] = {0, 0, 1, -1, 1, 1, -1, -1};
 int dkx8[] = {-1, 1, -1, 1, -2, -2, 2, 2};
-int dky8[] = {2, 2, -2, -2, 1, -1, 1, -1};
+int dky8[] = {2, 2, -2, -2, 1, -1, 1, -1};*/
 /* direction array */
 
 // Toh Chaliye Shuru karte...
